95d3fa27b53d02ccd82ade6043f7721894e50c2e
Eric Panorel <ericpanorel@aglux.ca>
2013-03-05 08:59:17 -0700

    Create ko-bootstrap-typeahead.js

---
+++ ko-bootstrap-typeahead.js
@@ -0,0 +1,31 @@
+/*!
+ * knockoutJs binding handler for Bootstrap TypeAhead plugin
+ * for asynchronous source
+ * by: Eric Panorel
+ * http://www.ericpanorel.net
+ */
+(function (ko, $) {
+    "use strict";
+    // binding handler
+    ko.bindingHandlers['TypeAhead'] = {
+        init: function (element, valueAccessor, allBindingsAccessor, context) {
+            var options = valueAccessor() || {};
+            var allBindings = allBindingsAccessor();
+            // update the "value" binding on select
+            var modelValue = allBindings.value;
+            if (modelValue) {
+                var handleValueChange = function (item) {
+                    var valueToWrite = item ? item : $(element).val();
+                    if (ko.isWriteableObservable(modelValue)) {
+                        modelValue(valueToWrite);
+                    }
+                    return item;
+                };
+                options.updater = handleValueChange;
+            }
+            // call bootstrap type ahead
+            $(element).typeahead(options);
+        }
+    };
+
+})(ko, jQuery);
